8d135b5098fe6f34e38a5d1ca16bf1a49c4d34d9
Tristan Smith <darcnite3000@gmail.com>
2018-07-11 14:38:02 +1000

    add locale

--- app/About.js
+++ app/About.js
@@ -3,12 +3,15 @@ import React from 'react'
 const About = () => (
   <div id="about-me">
     <h2>About Me</h2>
+    <p>Currently based in Ballina on the Far North Coast of NSW, Australia</p>
     <p>
       I've been working in the web industry for over 11 years and have seen the
       progression from Perl to PHP and from Flash to HTML5 ads, and am still
-      interested in the new front end technology coming out. I'm used to rapid
-      development, and emergency fixes in getting ads, sites and tools into
-      production.
+      interested in the new front end technology coming out.
+    </p>
+    <p>
+      I'm used to rapid development, and emergency fixes in getting ads, sites
+      and tools into production.
     </p>
   </div>
 )
