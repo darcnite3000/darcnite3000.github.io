5a1dfde185c611ff0dd73b9e52479aecb6e46cd4
Tristan Smith <darcnite3000@gmail.com>
2018-07-11 14:35:28 +1000

    move links

--- app/index.js
+++ app/index.js
@@ -12,38 +12,43 @@ const App = () => (
     <header>
       <h1>Tristan Smith</h1>
       <nav>
-        <a href="my_resume.pdf" download>
-          <ion-icon name="download" />Resume
+        <a href="https://www.linkedin.com/in/tristan-smith45/">
+          <ion-icon name="logo-linkedin" />
+        </a>
+        <a href="https://github.com/darcnite3000/">
+          <ion-icon name="logo-github" />
+        </a>
+        <a href="my_resume.pdf" title="Resume" download>
+          <ion-icon name="download" />R&#233;sum&#233;
         </a>
       </nav>
     </header>
     <div id="body">
-      <div id="contact">
-        <h2>Contact</h2>
-        <div class="list">
-          <a href="tel:+61431071276">
-            <ion-icon name="call" />+61431071276
-          </a>
-          <a href="mail:darcnite3000@gmail.com">
-            <ion-icon name="mail" />darcnite3000@gmail.com
-          </a>
-          <a href="https://www.linkedin.com/in/tristan-smith45/">
-            <ion-icon name="logo-linkedin" />https://www.linkedin.com/in/tristan-smith45/
-          </a>
-          <a href="https://github.com/darcnite3000/">
-            <ion-icon name="logo-github" />https://github.com/darcnite3000/
-          </a>
-          <a href="skype:tristan_smith45">
-            <ion-icon name="logo-skype" />tristan_smith45
-          </a>
-        </div>
-      </div>
       <About />
       <Education />
       <Work />
       <Hobbies />
       <Skills />
     </div>
+    <footer>
+      <div class="contact-list">
+        <a href="tel:+61431071276">
+          <ion-icon name="call" /> +61 431 071 276
+        </a>
+        <a href="mail:darcnite3000@gmail.com">
+          <ion-icon name="mail" /> darcnite3000@gmail.com
+        </a>
+        <a href="https://www.linkedin.com/in/tristan-smith45/">
+          <ion-icon name="logo-linkedin" /> tristan-smith45
+        </a>
+        <a href="https://github.com/darcnite3000/">
+          <ion-icon name="logo-github" /> darcnite3000
+        </a>
+        <a href="skype:tristan_smith45">
+          <ion-icon name="logo-skype" /> tristan_smith45
+        </a>
+      </div>
+    </footer>
   </div>
 )
 
